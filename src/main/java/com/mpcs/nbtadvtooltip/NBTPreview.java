3a60b3b7130fd515d5d4c81289579255e2a9155a
Mpcs <kuba.mpcs@gmail.com>
2021-04-18 04:18:15 +0200

    ver 1.1, supports chests etc. + better localisation + polish language

--- src/main/java/com/mpcs/nbtadvtooltip/NBTPreview.java
+++ src/main/java/com/mpcs/nbtadvtooltip/NBTPreview.java
@@ -1,10 +1,11 @@
 package com.mpcs.nbtadvtooltip;
 
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,8 +19,10 @@ import org.lwjgl.glfw.GLFW;
 public class NBTPreview {
 
     public static KeyBinding showNbtKeybinding;
+    public static boolean isKeybindPressed;
 
     public NBTPreview() {
+
         MinecraftForge.EVENT_BUS.register(this);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
     }
@@ -29,6 +32,52 @@ public class NBTPreview {
         if (itemTooltipEvent.getFlags().isAdvanced()) {
             ItemStack stack = itemTooltipEvent.getItemStack();
             if (stack.hasTag()) {
+                if (isKeybindPressed) {
+                    String tags = stack.getTag().toString();
+                    itemTooltipEvent.getToolTip().add(new StringTextComponent(tags));
+                } else {
+                    itemTooltipEvent.getToolTip().add(new TranslationTextComponent("tooltip.nbtadvtooltip.hold_key", showNbtKeybinding.func_238171_j_()));
+                }
+
+            } else {
+                itemTooltipEvent.getToolTip().add(new TranslationTextComponent("tooltip.nbtadvtooltip.no_nbt_found"));
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardKeyPressedEvent.Post event) {
+        InputMappings.Input input = InputMappings.getInputByCode(event.getKeyCode(), event.getScanCode());
+        if (showNbtKeybinding.isActiveAndMatches(input)) {
+            isKeybindPressed = true;
+        }
+    }
+
+    @SubscribeEvent
+    public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardKeyReleasedEvent.Post event) {
+        InputMappings.Input input = InputMappings.getInputByCode(event.getKeyCode(), event.getScanCode());
+        if (showNbtKeybinding.isActiveAndMatches(input)) {
+            isKeybindPressed = false;
+        }
+    }
+
+    private void clientSetup(FMLClientSetupEvent event) {
+        showNbtKeybinding = new KeyBinding("key.nbtadvtooltip.show_nbt.desc", GLFW.GLFW_KEY_LEFT_SHIFT, "key.nbtadvtooltip.category");
+        ClientRegistry.registerKeyBinding(showNbtKeybinding);
+    }
+}
+
+
+/*
+    @SubscribeEvent
+    public void ItemTooltipEvent(ItemTooltipEvent itemTooltipEvent) {
+        System.out.println("Start");
+        if (itemTooltipEvent.getFlags().isAdvanced()) {
+            System.out.println("Advanced");
+            ItemStack stack = itemTooltipEvent.getItemStack();
+            if (stack.hasTag()) {
+                System.out.print("Has Tag");
+                Keyboard
                 if (showNbtKeybinding.isKeyDown()) {
                     String tags = stack.getTag().toString();
                     itemTooltipEvent.getToolTip().add(new StringTextComponent(tags));
@@ -41,9 +90,4 @@ public class NBTPreview {
             }
         }
     }
-
-    private void clientSetup(FMLClientSetupEvent event) {
-        showNbtKeybinding = new KeyBinding("key.nbtadvtooltip.show_nbt.desc", GLFW.GLFW_KEY_LEFT_SHIFT, "key.nbtadvtooltip.category");
-        ClientRegistry.registerKeyBinding(showNbtKeybinding);
-    }
-}
+ */
