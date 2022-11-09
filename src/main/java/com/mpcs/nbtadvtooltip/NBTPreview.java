c88d4892c579e28516313d5599ef05097ff71d22
Wagyourtail <wagyourtail@wagyourtail.xyz>
2022-11-08 23:41:12 -0700

    update to 1.18.2

--- src/main/java/com/mpcs/nbtadvtooltip/NBTPreview.java
+++ src/main/java/com/mpcs/nbtadvtooltip/NBTPreview.java
@@ -1,15 +1,15 @@
 package com.mpcs.nbtadvtooltip;
 
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.client.util.InputMappings;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.client.event.GuiScreenEvent;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,7 +18,7 @@ import org.lwjgl.glfw.GLFW;
 @Mod("nbtadvtooltip")
 public class NBTPreview {
 
-    public static KeyBinding showNbtKeybinding;
+    public static KeyMapping showNbtKeybinding;
     public static boolean isKeybindPressed;
 
     public NBTPreview() {
@@ -33,36 +33,37 @@ public class NBTPreview {
             ItemStack stack = itemTooltipEvent.getItemStack();
             if (stack.hasTag()) {
                 if (isKeybindPressed) {
+                    assert stack.getTag() != null;
                     String tags = stack.getTag().toString();
-                    itemTooltipEvent.getToolTip().add(new StringTextComponent(tags));
+                    itemTooltipEvent.getToolTip().add(new TextComponent(tags));
                 } else {
-                    itemTooltipEvent.getToolTip().add(new TranslationTextComponent("tooltip.nbtadvtooltip.hold_key", showNbtKeybinding.func_238171_j_()));
+                    itemTooltipEvent.getToolTip().add(new TranslatableComponent("tooltip.nbtadvtooltip.hold_key", showNbtKeybinding.getTranslatedKeyMessage()));
                 }
 
             } else {
-                itemTooltipEvent.getToolTip().add(new TranslationTextComponent("tooltip.nbtadvtooltip.no_nbt_found"));
+                itemTooltipEvent.getToolTip().add(new TranslatableComponent("tooltip.nbtadvtooltip.no_nbt_found"));
             }
         }
     }
 
     @SubscribeEvent
-    public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardKeyPressedEvent.Post event) {
-        InputMappings.Input input = InputMappings.getInputByCode(event.getKeyCode(), event.getScanCode());
+    public void onGuiKeyboardEvent(ScreenEvent.KeyboardKeyPressedEvent.Post event) {
+        InputConstants.Key input = InputConstants.getKey(event.getKeyCode(), event.getScanCode());
         if (showNbtKeybinding.isActiveAndMatches(input)) {
             isKeybindPressed = true;
         }
     }
 
     @SubscribeEvent
-    public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardKeyReleasedEvent.Post event) {
-        InputMappings.Input input = InputMappings.getInputByCode(event.getKeyCode(), event.getScanCode());
+    public void onGuiKeyboardEvent(ScreenEvent.KeyboardKeyReleasedEvent.Post event) {
+        InputConstants.Key input = InputConstants.getKey(event.getKeyCode(), event.getScanCode());
         if (showNbtKeybinding.isActiveAndMatches(input)) {
             isKeybindPressed = false;
         }
     }
 
     private void clientSetup(FMLClientSetupEvent event) {
-        showNbtKeybinding = new KeyBinding("key.nbtadvtooltip.show_nbt.desc", GLFW.GLFW_KEY_LEFT_SHIFT, "key.nbtadvtooltip.category");
+        showNbtKeybinding = new KeyMapping("key.nbtadvtooltip.show_nbt.desc", GLFW.GLFW_KEY_LEFT_SHIFT, "key.nbtadvtooltip.category");
         ClientRegistry.registerKeyBinding(showNbtKeybinding);
     }
 }
